a0a89a3a0a66f85ed533ae34653550cebb1ac519
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-09-26 11:16:53 -0400

    Java Programming - Day01

---
+++ src/day01_introToProgramming/HelloWorld.java
@@ -0,0 +1,22 @@
+package day01_introToProgramming;  //This is package name
+
+public class HelloWorld {  // This is class header
+
+    public static void main(String[] args) { // This is main method (makes our java file runnable)
+
+        System.out.println("Hello  World");  // It prints "Hello World" on the console
+
+    }
+
+}
+
+/*
+Today I have learned:
+            1. Main Method
+            2. Print Statement
+            3. Execution mechanism
+            4. Components of Java
+
+        04/28/2022
+
+*/
