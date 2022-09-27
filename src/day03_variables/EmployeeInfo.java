e7e3ed21e6dd619f50bcc636d56d78cb998cb5bc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-09-27 09:33:36 -0400

    Java Programming - Day03

---
+++ src/day03_variables/EmployeeInfo.java
@@ -0,0 +1,78 @@
+package day03_variables;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        /*
+
+        System.out.println("Aaron");
+
+        System.out.println("Aaron");
+
+        System.out.println("Aaron");
+
+        System.out.println("--------------------------------");
+
+        String name = "Aaron";
+
+        System.out.println(name);
+
+        System.out.println(name);
+
+        System.out.println(name);
+        */
+
+
+        String employeeName = "Emily";
+        int age = 30;
+        char gender = 'F';
+        String companyName = "Google Inc";
+        String employeeId = "D11";
+        String jobTitle = "Scrum Master";
+        int salary = 110000;
+        boolean isFullTime = false;
+
+
+        System.out.println(employeeName);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(companyName);
+        System.out.println(employeeId);
+        System.out.println(jobTitle);
+        System.out.println(salary);
+        System.out.println(isFullTime);
+
+        System.out.println("-----------------------------------------------------");
+
+
+        System.out.println("employeeName = " + employeeName);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("companyName = " + companyName);
+        System.out.println("employeeId = " + employeeId);
+        System.out.println("jobTitle = " + jobTitle);
+        System.out.println("salary = " + salary);
+        System.out.println("isFullTime = " + isFullTime);
+
+
+
+
+
+
+
+    }
+
+}
+
+/*
+Declare the following variables with appropriate data types:
+name
+age
+gender
+companyName
+employeeId
+jobTitle
+salary
+isFullTime
+ */
---
+++ src/day03_variables/StudentInfo.java
@@ -0,0 +1,51 @@
+package day03_variables;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+        // DataType variableName = Data
+
+        /*
+        String studentName = "Abidullah",
+                school_nam = "Cydeo",
+                grade_level = "Mid Level JD";
+
+        int num1 = 100, num2 = 200, num3 =300;
+        */
+
+        String student_name = "Nora";
+        String school_name = "MIT";
+        String grade_level = "Undergraduate";
+        String student_id = "C19";
+
+        int age = 26;
+        char gender = 'F';
+        double gpa = 3.8;
+
+        boolean isFullTime = true;
+
+        System.out.println("student_name = " + student_name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("school_name = " + school_name);
+        System.out.println("grade_level = " + grade_level);
+        System.out.println("student_id = " + student_id);
+        System.out.println("gpa = " + gpa);
+        System.out.println("isFullTime = " + isFullTime);
+
+
+    }
+
+}
+
+/*
+Declare the following variables with appropriate data types:
+    students_name
+    age
+    gender
+    school_name
+    student_id
+    grade_level
+    gpa
+ */
